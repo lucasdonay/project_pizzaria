ac440ea74dd883e8b42c37df68f2a880b309a7b2
Lucas <lucas.donay@gmail.com>
2023-02-13 20:00:28 -0300

    studies day 04

--- process/pizza.php
+++ process/pizza.php
@@ -7,17 +7,17 @@ $method = $_SERVER["REQUEST_METHOD"];
 //RESGATE DOS DADOS, MONTAGEM DO PEDIDO
 if ($method === "GET") {
 
-  $bordasQuery = $conn->query("SELECT * FROM bordas;");
+    $bordasQuery = $conn->query("SELECT * FROM bordas;");
 
-  $bordas = $bordasQuery->fetchAll();
+    $bordas = $bordasQuery->fetchAll();
 
-  $massasQuery = $conn->query("SELECT * FROM massas;");
+    $massasQuery = $conn->query("SELECT * FROM massas;");
 
-  $massas = $massasQuery->fetchAll();
+    $massas = $massasQuery->fetchAll();
 
-  $saboresQuery = $conn->query("SELECT * FROM sabores;");
+    $saboresQuery = $conn->query("SELECT * FROM sabores;");
 
-  $sabores = $saboresQuery->fetchAll();
+    $sabores = $saboresQuery->fetchAll();
 } else if ($method === "POST") {
 
     $data = $_POST;
@@ -27,7 +27,7 @@ if ($method === "GET") {
     $sabores = $data["sabores"];
 
     //if de validacao de sabores maximos
-    if(count($sabores) > 3) {
+    if (count($sabores) > 3) {
         $_SESSION["msg"] = "Selecione no maximo 3 sabores!";
         $_SESSION["status"] = "warning";
     } else {
@@ -47,7 +47,35 @@ if ($method === "GET") {
         //resgatando ultimo dia da pizza
 
         $pizzaId = $conn->lastInsertId();
-        $stmt = $conn->prepare("INSERT INTO piziza")
+        $stmt = $conn->prepare("INSERT INTO pizza_sabor (id_pizza, id_sabor) VALUES (:pizza, :sabor)");
+
+
+        // repetição até terminar de salvar todos os sabores
+
+        foreach ($sabores as $sabor) {
+            //filtrando os inputs
+
+            $stmt->bindParam(":pizza", $pizzaId, PDO::PARAM_INT);
+            $stmt->bindParam(":sabor", $sabor, PDO::PARAM_INT);
+
+            $stmt->execute();
+        };
+
+        // criar o pedido da pizza
+
+        $stmt = $conn->prepare("INSERT INTO pedidos (id_pizza, id_status) VALUES (:pizza,:status)");
+
+        // status sempre inicia com 1 que é de produção
+
+        $statusId = 1;
+
+        // filtrar inputs
+
+        $stmt->bindParam(":pizza", $pizzaId);
+        $stmt->bindParam(":status", $statusId);
+
+        $stmt->execute();
+
 
         $_SESSION["msg"] = "Pedido realizado com sucesso!";
         $_SESSION["status"] = "success";
@@ -55,4 +83,3 @@ if ($method === "GET") {
 
     header("Location: ..");
 }
-
